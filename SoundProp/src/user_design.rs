8d0b47467bd802017f7ba50a042fdff8f30bb4d8
Jack Murdoch <101184324+JackMurds@users.noreply.github.com>
2024-04-12 15:50:18 +0100

    Update user_design.rs
    Controlled user visualisation

--- SoundProp/src/user_design.rs
+++ SoundProp/src/user_design.rs
@@ -5,14 +5,14 @@ use crate::ray_trace::{self, Rays};
 pub const PI: f64 = 3.14159265358979323846264338327950288_f64;
 use std::io::prelude::*;
 
-pub enum Source_Type {
-    point,
-    line,
+pub enum SourceType {
+    Point,
+    Line,
 }
 
-
+//                                               MARK: Simulation Struct
 pub struct Simulation {
-    pub dt : f64,
+    dt : f64,
     sources : Vec<Source>,
     //pub water_type : somefunction,
     //pub boundary_type: somefunction,
@@ -31,7 +31,7 @@ impl Simulation {
         }
     }
 
-    pub fn add_source(&mut self, min_angle: f64, max_angle: f64, number_of_rays: i32, intensity: f64, frequency: f64, location: [f64;2], source_type: Source_Type) -> () {
+    pub fn add_source(&mut self, min_angle: f64, max_angle: f64, number_of_rays: i32, intensity: f64, frequency: f64, location: [f64;2], source_type: SourceType) -> () {
         if min_angle >= max_angle {
             eprintln!("Error: Minimum angle must be less than the maximum angle.");
             std::process::exit(1);
@@ -42,7 +42,7 @@ impl Simulation {
         } // Checks if the minimum and maximum angles are within the range of +/- PI.
 
         let new_source: Source = Source::initialise(source_type, min_angle, max_angle, 
-            (number_of_rays as usize), intensity, frequency, location);
+            number_of_rays as usize, intensity, frequency, location);
         
         self.sources.push(new_source);
         // Adds new source to an array of sources under the Simulation struct.
@@ -54,34 +54,35 @@ impl Simulation {
             std::process::exit(1);
         } // Ensures that a source has been defined prior to this function.
         
-        let SIZE: i32 = (duration / self.dt) as i32;
+        let size: i32 = (duration / self.dt) as i32;
         let number_of_rays: usize = self.sources.iter().map(|source| source.number_of_rays as usize).sum();
         //Sums 'number_of_rays' across all sources.
 
-        let mut x_output: Vec<Vec<f64>> = Vec::with_capacity(SIZE as usize);
-        let mut y_output: Vec<Vec<f64>> = Vec::with_capacity(SIZE as usize);
+        let mut x_output: Vec<Vec<f64>> = Vec::with_capacity(size as usize);
+        let mut y_output: Vec<Vec<f64>> = Vec::with_capacity(size as usize);
         // Allocates the x and y ouputs. Inner vector indexes ray count, outer vector indexes time.
 
-        let mut Rays: Rays = ray_trace::Rays::initialise(number_of_rays);
+        let mut rays: Rays = ray_trace::Rays::initialise(number_of_rays);
         // Defines the Rays struct with each variable inside having an appendable vector with minimum array size (beneficial for memory).
         
         for i in 0..self.sources.len() {
-            self.sources[i].create_rays(&mut Rays, self.dt);
+            self.sources[i].create_rays(&mut rays, self.dt);
         } // Compiles all of the initial data for each ray, from its sources, into one 'Rays' struct.
 
-        Rays.boundAngles(self.dt);
+        rays.bound_angles(self.dt);
 
-        for i in 0..SIZE-1 {
+        for i in 0..size-1 {
             if i != 0{
-              Rays.step();  
+              rays.step();  
             } // Done to ensure that the initial positions of the rays is not overwritten in the output file.
-            x_output.push( Rays.x_pos.clone() );
-            y_output.push( Rays.y_pos.clone() );
+            let (x_pos, y_pos) = rays.output_position();
+            x_output.push( x_pos );
+            y_output.push( y_pos );
         }
         self.output(x_output, y_output);
     }
 
-    pub fn output(&mut self, xdata: Vec<Vec<f64>>, ydata: Vec<Vec<f64>>) {
+    fn output(&mut self, xdata: Vec<Vec<f64>>, ydata: Vec<Vec<f64>>) {
         if xdata.len() != ydata.len() {
             eprintln!("Error: Length of 'xdata' array does not match with length of 'ydata' array.");
             std::process::exit(1);
@@ -121,9 +122,9 @@ impl Simulation {
 }
 
 
-
+//                                                 MARK: Source Struct
 pub struct Source {
-    source_type : Source_Type,
+    source_type : SourceType,
     angle_range : [f64;2],
     number_of_rays : usize,
     intensity : f64,
@@ -132,7 +133,7 @@ pub struct Source {
 }
 
 impl Source {
-    pub fn initialise(source_type: Source_Type, min_angle: f64, max_angle: f64,
+    pub fn initialise(source_type: SourceType, min_angle: f64, max_angle: f64,
     number_of_rays: usize, intensity: f64, frequency: f64, location: [f64;2]) -> Self {
         Self {
             source_type : source_type,
@@ -147,7 +148,7 @@ impl Source {
     pub fn create_rays(&mut self, initial_rays: &mut Rays, dt: f64) {
         let mut initial_angles: Vec<f64> = Vec::with_capacity(self.number_of_rays);
         match self.source_type {
-            Source_Type::point => {
+            SourceType::Point => {
                 let angle_spacing: f64 = (self.angle_range[1] - self.angle_range[0]) / (self.number_of_rays as f64);
 
                 for i in 0..self.number_of_rays-1 {
@@ -166,7 +167,7 @@ impl Source {
                     vec![self.frequency;self.number_of_rays-1],
                     vec![dt;self.number_of_rays-1])
             }
-            Source_Type::line => {
+            SourceType::Line => {
                 println!("Not yet implemented");
             }
         } 
