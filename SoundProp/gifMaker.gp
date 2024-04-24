9ce39e3e08da943883a765cb044f4b0817e05663
JackMurds <J.Murdoch-21@student.lboro.ac.uk>
2024-04-24 02:06:29 +0100

    -Moved code from user_design into ray_trace -Allowed gif to plot intensity as a heat map

--- SoundProp/gifMaker.gp
+++ SoundProp/gifMaker.gp
@@ -10,11 +10,14 @@ outfile = sprintf("%s%s%s",".\\outputImages", "\\imageGif", ".gif")
 
 set xrange [-1500 : 1500]
 set yrange [-2000 : 1000]
+set cbrange [0.996:1]  # Set the color range from 0.966 to 1
 
 
 set out outfile 
 
-print limit
+limit = 100
 
+do for [i = 0: limit-1] {plot infile.i.".txt" using 1:2:3 with points pt 7 ps 1 palette title sprintf("Time: %.2f", i * 0.01)}
 
-do for [i = 1: limit - 1] {plot infile.i.".txt"}
+# We should aim to allow the following variables to be passed in: xrange, yrange, number of files, intensity range, time between frames
+# Currently alternatives have been hard-coded
--- SoundProp/src/main.rs
+++ SoundProp/src/main.rs
@@ -1,9 +1,8 @@
-use user_design::Simulation;
-use crate::user_design::SourceType::Point;
+use ray_trace::Simulation;
+use crate::ray_trace::SourceType::Point;
 
 
 mod ray_trace;
-mod user_design;
 // Inputs our 'ray_trace' module to this file.
 
 pub const PI: f64 = 3.14159265358979323846264338327950288_f64;
@@ -11,7 +10,8 @@ pub const PI: f64 = 3.14159265358979323846264338327950288_f64;
 fn main() -> std::io::Result<()> {
 
     let mut sound_prop: Simulation = Simulation::initialise(0.01);
-    sound_prop.add_source(-PI, PI, 240, 1.0, 20.0, [0.0, -100.0], Point);
+    sound_prop.add_source(-PI, PI, 240, 1.0, 20.0, [-500.0, -100.0], Point);
+    sound_prop.add_source(-PI, PI, 240, 1.0, 20.0, [500.0, -100.0], Point);
     sound_prop.calculate(5.0);
     sound_prop.gif();
 
