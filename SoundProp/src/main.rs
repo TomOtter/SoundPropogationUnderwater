a563682313c4e88a69defe6b0317d75e857fbbe7
JackMurds <J.Murdoch-21@student.lboro.ac.uk>
2024-04-26 01:26:50 +0100

    -Rays can interfere (currently only constructive) -Directories now automatically delete all contents -Data points outside simulation are discarded -Number of files/frames is now user defined Code still runs reasonably fast

--- SoundProp/src/main.rs
+++ SoundProp/src/main.rs
@@ -9,11 +9,10 @@ pub const PI: f64 = 3.14159265358979323846264338327950288_f64;
 
 fn main() -> std::io::Result<()> {
 
-    let mut sound_prop: Simulation = Simulation::initialise(0.01);
+    let mut sound_prop: Simulation = Simulation::initialise(0.001, 1.0, [-1500.0,1500.0], [-2000.0,1000.0]);
     sound_prop.add_source(-PI, PI, 240, 1.0, 20.0, [-500.0, -100.0], Point);
     sound_prop.add_source(-PI, PI, 240, 1.0, 20.0, [500.0, -100.0], Point);
-    sound_prop.calculate(5.0);
-    sound_prop.gif();
+    sound_prop.gif(2.0, 100);
 
     Ok(())
     
--- SoundProp/src/ray_trace.rs
+++ SoundProp/src/ray_trace.rs
@@ -1,5 +1,6 @@
 use core::num;
 use std::{
+    collections::HashMap,
     fs::{self, File},
     io::prelude::*,
     path::Path,
@@ -17,21 +18,23 @@ pub enum SourceType {
 pub struct Simulation {
     dt : f64,
     sources : Vec<Source>,
+    grid: Grid,
     //pub water_type : somefunction,
     //pub boundary_type: somefunction,
 }
 
 impl Simulation {
 
-    pub fn initialise(dt: f64) -> Self {
+    pub fn initialise(dt: f64, square_size: f64, simulation_x_range: [f64;2], simulation_y_range: [f64;2]) -> Self {
         if dt <= 0.0 {
             eprintln!("Error: dt must be a positive, non-zero, float.");
             std::process::exit(1);
         } // Ensures that dt is a positive, non-zero, float.
-
+        let grid = Grid::initialise(square_size, simulation_x_range, simulation_y_range);
         Self {
             dt : dt,
             sources : Vec::new(),
+            grid: grid,
         }
     } // Initialisation function to define the fields inside of Simulation after undergoing necessary error checks.
 
@@ -52,20 +55,19 @@ impl Simulation {
         // Adds new source to an array of sources under the Simulation struct.
     }
 
-    pub fn calculate(&mut self, duration: f64) -> () {
+    fn calculate(&mut self, duration: f64, frames: i32) -> () {
         if self.sources.len() == 0 {
             eprintln!("Error: No sources have been defined. Call 'self.addSource' prior to this function to define a soundwave source.");
             std::process::exit(1);
         } // Ensures that a source has been defined prior to this function.
         
+        self.create_folder("./outputdata");
+        // Creates folder for data files to be stored
+
         let size: i32 = (duration / self.dt) as i32;
+        let frame_spacing: i32 = size / frames;
         let number_of_rays: usize = self.sources.iter().map(|source| source.number_of_rays as usize).sum();
         //Sums 'number_of_rays' across all sources.
-
-        let mut x_output: Vec<Vec<f64>> = Vec::with_capacity(size as usize);
-        let mut y_output: Vec<Vec<f64>> = Vec::with_capacity(size as usize);
-        let mut intensity_output: Vec<Vec<f64>> = Vec::with_capacity(size as usize);
-        // Allocates the x and y ouputs. Inner vector indexes ray count, outer vector indexes time.
 
         let mut rays: Rays = Rays::initialise(number_of_rays);
         // Defines the Rays struct with each variable inside having an appendable vector with minimum array size (beneficial for memory).
@@ -78,61 +80,59 @@ impl Simulation {
 
         for i in 0..size {
             if i != 0{
-              rays.step();  
+                self.grid.squares.clear();
+                rays.step(self.grid.x_range, self.grid.y_range);  
             } // Done to ensure that the initial positions of the rays is not overwritten in the output file.
-            let (x_pos, y_pos) = rays.output_position();
-            let intensity = rays.output_intensity();
-            x_output.push( x_pos );
-            y_output.push( y_pos );
-            intensity_output.push( intensity );
+            if (i % frame_spacing) == 0 {
+                let (x_pos, y_pos) = rays.output_position();
+                let intensity = rays.output_intensity();
+                for j in 0..x_pos.len() {
+                    self.grid.append([x_pos[j], y_pos[j]], intensity[j]);
+                }
+                self.output(i / frame_spacing);
+            }
         } // Time loop which pushes each ray by one step and outputs the new positions each iteration.
-        self.output(x_output, y_output, intensity_output);
     }
 
 //                                                    MARK: Outputs
 
-    fn create_folder(&mut self, folder_path: &str) -> () {
-        if !Path::new(folder_path).exists() {
-            match std::fs::create_dir(folder_path) {
-                Ok(_) => {},
+    fn create_folder(&mut self, folder_path: &str) {
+        let path = Path::new(folder_path);
+        if path.exists() {
+            drop(fs::read_dir(folder_path));
+            match fs::remove_dir_all(folder_path) {
+                Ok(_) => (),
                 Err(err) => {
-                    eprintln!("Error creating directory {}: {}", folder_path, err);
+                    eprintln!("Error deleting directory {}: {}", folder_path, err);
                     std::process::exit(1);
                 }
+            } // Close and delete the directory
+        }
+        match fs::create_dir(folder_path) {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("Error creating directory {}: {}", folder_path, err);
+                std::process::exit(1);
             }
-        } // Check if the folder exists, if not creates it
+        } // Create the new directory
     }
 
-    fn output(&mut self, xdata: Vec<Vec<f64>>, ydata: Vec<Vec<f64>>, intensity_data: Vec<Vec<f64>>) {
-        if xdata.len() != ydata.len() {
-            eprintln!("Error: Length of 'xdata' array does not match with length of 'ydata' array.");
-            std::process::exit(1);
-        } // Ensure the length of 'xdata' matches with 'ydata'
-    
-        for i in 0..xdata.len() {
-            if xdata[i].len() != ydata[i].len() {
-                eprintln!("Error: Length of 'xdata{0}' array does not match with length of 'ydata{0}' array.", i);
-                std::process::exit(1);
-            } // Ensure the length of 'xdata[i]' matches with 'ydata[i]'
-    
+    fn output(&mut self, file_count: i32) -> () {
+        let (xpos, ypos, intensity) = self.grid.output_data();
+
             let mut output = String::new();
             // Create a string to hold the output for this iteration
-    
-            for j in 0..xdata[i].len() {
-                let xpos: f64 = xdata[i][j];
-                let ypos: f64 = ydata[i][j];
-                let intensity: f64 = intensity_data[i][j];
-                output.push_str(&format!("{} {} {}\n", xpos, -1.0 * ypos, intensity));
+
+            for i in 0..xpos.len() {
+                output.push_str(&format!("{} {} {}\n", xpos[i], -1.0 * ypos[i], intensity[i]));
                 // Append position data to the output string
 
             } // Loop through each position in the current time step
-    
+
             let folder_path = "./outputdata";
             // Define the folder path where output files will be stored
 
-            self.create_folder(folder_path);
-
-            let file_name = format!("{}/dataset{}.txt", folder_path, i);
+            let file_name = format!("{}/dataset{}.txt", folder_path, file_count);
             // Define the file name with the folder path and the index 'i'
     
             let mut file = match File::create(&file_name) {
@@ -147,11 +147,10 @@ impl Simulation {
                 eprintln!("Error writing to file {}: {}", &file_name, err);
                 std::process::exit(1);
             } // Write the output string to the file
-
-        } // Loop through each time step
     }
 
-    pub fn gif(&mut self) -> Output {
+    pub fn gif(&mut self, duration: f64, frames: i32) -> Output {
+        self.calculate(duration, frames);
         let txt_files = match fs::read_dir("outputdata") {
             Ok(entries) => {
                 entries.filter_map(|entry| {
@@ -182,8 +181,6 @@ impl Simulation {
         let length = txt_files.len();
         let cmd = format!("runGifMAker.bat {} ",length );
 
-
-    
         if cfg!(target_os = "windows") {
             Command::new("cmd")
                 .args(["/C", &cmd ])
@@ -196,9 +193,6 @@ impl Simulation {
                 .output()
                 .expect("failed to execute process")
         }
-
-
-
         // Execute the command to generate the GIF, with OS check
     }
 }
@@ -249,15 +243,12 @@ impl Source {
                     initial_angles.push( self.angle_range[0] + (angle_spacing * i as f64) );
                 } // Evenly spaces out the arrays at the source between the given bounds and appends to the initial ray angles struct.
 
-                /* initial_rays.x_pos.extend( vec![self.location[0];self.number_of_rays-1] );
-                initial_rays.y_pos.extend( vec![-1.0 * self.location[1];self.number_of_rays-1] );
-                initial_rays.intensity.extend( vec![self.intensity;self.number_of_rays-1] );
-                initial_rays.step_vector.extend( vec![dt;self.number_of_rays-1] );
-                initial_rays.frequency.extend( vec![self.frequency;self.number_of_rays-1] ); */
+                let local_ray_intensity: f64 = self.intensity / f64::powi(self.number_of_rays as f64, 2);
+
                 initial_rays.create_rays(initial_angles,
                     vec![self.location[0] ; self.number_of_rays],
                     vec![-1.0 * self.location[1];self.number_of_rays],
-                    vec![self.intensity;self.number_of_rays],
+                    vec![local_ray_intensity;self.number_of_rays],
                     vec![self.frequency;self.number_of_rays],
                     vec![dt;self.number_of_rays])
             }
@@ -280,31 +271,43 @@ pub struct Rays {
 } // Defines the properties of each ray.
 
 impl Rays {
-    pub fn step(&mut self) {
+    pub fn step(&mut self, simulation_x_limit: [f64;2], simulation_y_limit: [f64;2]) -> () {
         let mut new_x_pos: f64;
         let mut new_y_pos: f64;
-        for i in 0..self.x_pos.len() {
-            new_x_pos = self.x_pos[i] + self.step_vector[i] * material_speed(self.y_pos[i],self.x_pos[i]) * self.angle[i].sin();
-            new_y_pos = self.y_pos[i] + self.step_vector[i] * material_speed(self.y_pos[i],self.x_pos[i]) * self.angle[i].cos();
-            // Caluclates the new position of each ray after 1 time step
+        let mut i: usize = 0;
 
-            if material_speed(new_y_pos, new_x_pos) > material_speed(self.y_pos[i], self.x_pos[i]) {
-                let critical_angle : f64 = (material_speed(self.y_pos[i], self.x_pos[i])/material_speed(new_y_pos, new_x_pos)).asin();
-                if self.angle[i].abs() > critical_angle.abs() {
-                    self.angle[i] = -1.0 * self.angle[i];
-                    self.step_vector[i] = self.step_vector[i] * -1.0;
+        while i != self.x_pos.len() {
+            if (self.x_pos[i] < simulation_x_limit[0]) || (self.x_pos[i] > simulation_x_limit[1]) || (-self.y_pos[i] < simulation_y_limit[0])  || (-self.y_pos[i] > simulation_y_limit[1]) {
+                self.angle.remove(i);
+                self.x_pos.remove(i);
+                self.y_pos.remove(i);
+                self.intensity.remove(i);
+                self.step_vector.remove(i);
+                self.frequency.remove(i);
+            } else { 
+                new_x_pos = self.x_pos[i] + self.step_vector[i] * material_speed(self.y_pos[i],self.x_pos[i]) * self.angle[i].sin();
+                new_y_pos = self.y_pos[i] + self.step_vector[i] * material_speed(self.y_pos[i],self.x_pos[i]) * self.angle[i].cos();
+                // Caluclates the new position of each ray after 1 time step
+
+                if material_speed(new_y_pos, new_x_pos) > material_speed(self.y_pos[i], self.x_pos[i]) {
+                    let critical_angle : f64 = (material_speed(self.y_pos[i], self.x_pos[i])/material_speed(new_y_pos, new_x_pos)).asin();
+                    if self.angle[i].abs() > critical_angle.abs() {
+                        self.angle[i] = -1.0 * self.angle[i];
+                        self.step_vector[i] = self.step_vector[i] * -1.0;
+                    }
+                    // Reflects the ray if its angle with the normal exceeds the critical angle.
                 }
-                // Reflects the ray if its angle with the normal exceeds the critical angle.
+                // Implement some if statement around here for reflection with boundary.
+
+                let preangle = material_speed(new_y_pos, new_x_pos)/material_speed(self.y_pos[i], self.x_pos[i]) * self.angle[i].sin();
+                self.x_pos[i] = new_x_pos;
+                self.y_pos[i] = new_y_pos;
+                self.angle[i] = preangle.asin();
+
+                let salinity = 35.0;
+                self.intensity[i] *= 1.0 - calculate_absorption(self.frequency[i], temperature_at_depth(self.y_pos[i]), salinity, self.y_pos[i]);
+                i += 1;
             }
-            // Implement some if statement around here for reflection with boundary.
-
-            let preangle = material_speed(new_y_pos, new_x_pos)/material_speed(self.y_pos[i], self.x_pos[i]) * self.angle[i].sin();
-            self.x_pos[i] = new_x_pos;
-            self.y_pos[i] = new_y_pos;
-            self.angle[i] = preangle.asin();
-
-            let salinity = 35.0;
-            self.intensity[i] = 1.0 - calculate_absorption(self.frequency[i], temperature_at_depth(self.y_pos[i]), salinity, self.y_pos[i])
         }
     }
 
@@ -359,6 +362,86 @@ impl Rays {
     pub fn output_intensity(&self) -> Vec<f64> {
         self.intensity.clone()
     } // Outputs a copy of each rays x and y position - to be used in functions implemented in other structs.
+}
+
+//                                                  MARK: Grid Struct
+
+pub struct Grid {
+    squares: HashMap<(usize, usize), Option<f64>>,
+    x_range: [f64;2],
+    y_range: [f64;2],
+    square_size: f64,
+}
+
+impl Grid {
+
+    pub fn initialise(square_size: f64, simulation_x_range: [f64;2], simulation_y_range: [f64;2]) -> Self {
+        if square_size <= 0.0 {
+            eprintln!("Error: square_size must be a positive, non-zero, integer.");
+            std::process::exit(1);
+        }
+        if simulation_x_range[0] >= simulation_x_range[1] {
+            eprintln!("Error: simulation_x_range[0] must be less than simulation_x_range[1].");
+            std::process::exit(1);
+        }
+        if simulation_y_range[0] >= simulation_y_range[1] {
+            eprintln!("Error: simulation_y_range[0] must be less than simulation_y_range[1].");
+            std::process::exit(1);
+        }
+        Self {
+            squares: HashMap::new(),
+            x_range: simulation_x_range,
+            y_range: simulation_y_range,
+            square_size: square_size,
+        }
+    }
+
+    pub fn append(&mut self, location: [f64; 2], intensity: f64) {
+        let x_grid: usize = ( (location[0] - self.x_range[0] ) / self.square_size) as usize;
+        let y_grid: usize = ( (location[1] - self.y_range[0] ) / self.square_size) as usize;
+        let phase_difference: f64 = 0.0;
+    
+        // Get a mutable reference to the intensity value in the grid square
+        if let Some(existing_intensity) = self.squares.get_mut(&(x_grid, y_grid)) {
+            // Check if the grid square already contains an intensity value
+            match existing_intensity {
+                // If the intensity value exists, update it
+                Some(intensity_ref) => {
+                    let old_intensity: f64 = *intensity_ref;
+                    let new_intensity: f64 = old_intensity + intensity +
+                        2.0 * (old_intensity * intensity).sqrt() * (phase_difference).cos();
+                    *intensity_ref = new_intensity;
+                },
+                // If the intensity value doesn't exist, insert the new intensity directly
+                None => {
+                    *existing_intensity = Some(intensity);
+                }
+            }
+        } else {
+            // If the grid square doesn't exist, create it and insert the new intensity
+            self.squares.insert((x_grid, y_grid), Some(intensity));
+        }
+    }
+
+    pub fn output_data(&self) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
+        let mut x_positions = Vec::new();
+        let mut y_positions = Vec::new();
+        let mut intensities = Vec::new();
+
+        // Iterate over the grid squares
+        for ((x, y), intensity_option) in &self.squares {
+            // Check if the grid square contains an intensity value
+            if let Some(intensity) = intensity_option {
+                // Push x position, y position, and intensity into their respective vectors
+                x_positions.push((*x as f64) + self.x_range[0]);
+                y_positions.push((*y as f64) + self.y_range[0]);
+                intensities.push(*intensity);
+            }
+        }
+
+        // Return a tuple containing the vectors of x positions, y positions, and intensities
+        (x_positions, y_positions, intensities)
+    }
 }
 
 
